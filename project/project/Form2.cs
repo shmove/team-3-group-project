269b61d37c670b1964d6b80c605864a19a6051f5
Destiny Uduebholo <obose03@gmail.com>
2021-01-14 14:12:53 +0000

    Login

---
+++ project/project/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = txtUser.Text;
+            pass = txtPassword.Text;
+
+            if (user == "admin" && pass == "password")
+            {
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("password is incorrect");
+            }
+        }
+    }
+}
+
