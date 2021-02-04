1ea6a2f0608a01014889fe817577b0885320376c
Permille <67966018+Permille@users.noreply.github.com>
2021-02-04 19:26:36 +0000

    Fixed stupid design decision I made 3 months ago
    Every Note has its date, instead of it being grouped by the date. The Notes have been made obsolete.

--- project/project/PupilDataManager/SharedResources/Types.cs
+++ project/project/PupilDataManager/SharedResources/Types.cs
@@ -17,28 +17,14 @@ namespace project.PupilDataManager.SharedResources {
                     else throw new FieldAccessException("Tried re-setting the UUID of a pupil.");
                 }
             }
-            public string PupilID {
-                get; set;
-            }
-            public string Name {
-                get; set;
-            }
-            public string Company {
-                get; set;
-            }
-            public bool A2E {
-                get; set;
-            }
-            public List<Notes> Notes {
-                get; set;
-            }
-            public string ImgRef {
-                get; set;
-            }
-            public Pupil() {
-
-            }
-            public Pupil(string p_Name, string p_PupilID, string p_Company, bool p_A2E, List<Notes> p_Notes, string p_ImgRef) {
+            public string PupilID {get; set;}
+            public string Name {get; set;}
+            public string Company {get; set;}
+            public bool A2E {get; set;}
+            public List<Note> Notes {get; set;}
+            public string ImgRef {get; set;}
+            public Pupil() {}
+            public Pupil(string p_Name, string p_PupilID, string p_Company, bool p_A2E, List<Note> p_Notes, string p_ImgRef) {
                 PupilUUID = System.Guid.NewGuid().ToString();
                 Name = p_Name;
                 A2E = p_A2E;
@@ -47,21 +33,23 @@ namespace project.PupilDataManager.SharedResources {
                 ImgRef = p_ImgRef;
                 PupilID = p_PupilID;
             }
-            public Pupil(object Object) {
-
-            }
+            public Pupil(object Object) {}
         }
-
+        [Obsolete("Old notes version, don't use this.")]
         public class Notes {
-            public string Date {
-                get; set;
-            }
-            public List<string> NotesList {
-                get; set;
-            }
+            public string Date {get; set;}
+            public List<string> NotesList {get; set;}
             public Notes(string p_Date, List<string> p_NotesList) {
                 Date = p_Date;
                 NotesList = p_NotesList;
+            }
+        }
+        public class Note {
+            public string Date {get; set;}
+            public string Text {get; set;}
+            public Note(string p_Date, string p_Text){
+                this.Date = p_Date;
+                this.Text = p_Text;
             }
         }
     }
