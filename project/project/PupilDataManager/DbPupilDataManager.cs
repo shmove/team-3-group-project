9a1be749957eb70ffad7e66ea5561cd691cc3ae4
Permille <67966018+Permille@users.noreply.github.com>
2021-02-11 13:54:03 +0000

    Version 0.1.0.3 of DbPupilDataManager
    Implemented interface IPupilDataManager. The only thing that's missing is note management and multi-user support.

--- project/project/PupilDataManager/DbPupilDataManager.cs
+++ project/project/PupilDataManager/DbPupilDataManager.cs
@@ -7,18 +7,20 @@ using System.Data;
 using System.Data.OleDb;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using static project.PupilDataManager.SharedResources.Types;
 
 namespace project.PupilDataManager {
     class DbPupilDataManager : BasePupilDataManager {
-        public static readonly string VERSION = "0.0.2.2";
-        public static readonly int BUILD = 2;
+        public static readonly string VERSION = "0.1.0.3";
+        public static readonly int BUILD = 3;
         private static readonly string DEFAULT_DATABASE_LOCATION = Environment.GetEnvironmentVariable("LocalAppData") + "\\PupilRecordsProgram\\Databases";
         private static readonly string RELATIVE_PUPIL_PICTURES_LOCATION = "\\Pictures";
         private static readonly string DATABASE_NAME = "PleaseDontDeleteThis";
         private static readonly string CONNECTION_STRING_TEMPLATE = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = ";
+        private static readonly string[] PUPIL_TABLE_COLUMN_NAMES = new string[]{"PupilUUID", "PupilID", "Name", "Company", "A2E", "ImgRef"};
         public string DatabasePath;
         public DbPupilDataManager(string p_DatabasePath) : base(p_DatabasePath) {
             DatabasePath = p_DatabasePath;
@@ -32,7 +34,7 @@ namespace project.PupilDataManager {
             if (Directory.Exists(Location)) {
                 try {
                     Catalog v_Catalog = new Catalog();
-                    v_Catalog.let_ActiveConnection(CONNECTION_STRING_TEMPLATE + Location);
+                    v_Catalog.let_ActiveConnection(CONNECTION_STRING_TEMPLATE + Location + "\\" + DATABASE_NAME);
                     //TODO: Check database integrity.
                     v_Catalog = null;
                 } catch (Exception) {
@@ -81,7 +83,7 @@ namespace project.PupilDataManager {
             });
 
             NotesTable.Keys.Append("ForeignKey", KeyTypeEnum.adKeyForeign, "PupilUUID", "Pupil", "PupilUUID");
-            PupilsTable.Keys.Append("PrimaryKey", ADOX.KeyTypeEnum.adKeyPrimary, "UUID", null, null);
+            NotesTable.Keys.Append("PrimaryKey", ADOX.KeyTypeEnum.adKeyPrimary, "UUID", null, null);
             v_Catalog.Tables.Append(NotesTable);
 
             Table MetadataTable = new Table();
@@ -112,20 +114,106 @@ namespace project.PupilDataManager {
         }
 
         public override List<T> CollatePropertyValuesFromPupils<T>(string Property) {
-            throw new NotImplementedException();
+            OleDbConnection Connection = new OleDbConnection(CONNECTION_STRING_TEMPLATE + this.DatabasePath + "\\" + DATABASE_NAME);
+
+            OleDbCommand SelectCommand = new OleDbCommand();
+            SelectCommand.CommandType = CommandType.Text;
+            SelectCommand.Connection = Connection;
+            //SelectCommand.CommandText = "SELECT @Property FROM [Pupil];";
+            //SelectCommand.Parameters.AddWithValue("@Property", Property); //This doesn't work for what ever reason: DataReader[0] always returns the Property name, instead of the value. Simply concatenating the table name works, but is unsafe.
+            SelectCommand.CommandText = "SELECT * FROM [Pupil];";
+
+            List<T> PropertyValues = new List<T>();
+
+            int Index = Array.IndexOf(PUPIL_TABLE_COLUMN_NAMES, Property);
+            if(Index == -1) return PropertyValues;
+
+            Connection.Open();
+            OleDbDataReader DataReader = SelectCommand.ExecuteReader();
+
+            while(DataReader.Read()) if(!PropertyValues.Contains((T)DataReader[Index])) PropertyValues.Add((T)DataReader[Index]);
+
+            Connection.Close();
+            return PropertyValues;
         }
 
         public override bool ConfirmPupilIDIntegrity() {
-            throw new NotImplementedException();
+            List<Pupil> Pupils = this.GetPupilsByProperties(new {});
+            List<string> PupilIDs = new List<string>();
+            foreach(Pupil i_Pupil in Pupils){
+                if(PupilIDs.Contains(i_Pupil.PupilID)) return false;
+                PupilIDs.Add(i_Pupil.PupilID);
+            }
+            return true;
         }
-
+        private static string[] CHECK_NESTED_MATCH_PROPERTY_BLACKLIST = { "Length", "Chars" };
         public override List<Pupil> GetPupilsByProperties(object Properties) {
-            throw new NotImplementedException();
+            PropertyInfo[] PropertyInfoArray = Properties.GetType().GetProperties();
+            List<(string, object)> StringProperties = new List<(string, object)>();
+            foreach (PropertyInfo Property in Properties.GetType().GetProperties()) {
+                if (CHECK_NESTED_MATCH_PROPERTY_BLACKLIST.Contains(Property.Name)) continue;
+                dynamic PatternValue = Properties.GetType().GetProperty(Property.Name).GetValue(Properties, null);
+                if (PatternValue == null) continue;
+                StringProperties.Add((Property.Name, PatternValue));
+            }
+
+            OleDbConnection Connection = new OleDbConnection(CONNECTION_STRING_TEMPLATE + this.DatabasePath + "\\" + DATABASE_NAME);
+
+            OleDbCommand SelectCommand = new OleDbCommand();
+            SelectCommand.CommandType = CommandType.Text;
+            SelectCommand.Connection = Connection;
+            string CommandText = "SELECT * FROM [Pupil]";
+            if(StringProperties.Count != 0) {
+                for (int i = 0; i < StringProperties.Count; i++){
+                    (string, object) StringProperty = StringProperties[i];
+
+                    if(i == 0) CommandText += " WHERE ";
+                    else CommandText += " AND ";
+                    CommandText += StringProperty.Item1 + " = @Value" + i;
+                }
+            }
+            CommandText += ";";
+            SelectCommand.CommandText = CommandText;
+
+            if(StringProperties.Count != 0) {
+                for(int i = 0; i < StringProperties.Count; i++){
+                    (string, object) StringProperty = StringProperties[i];
+                    SelectCommand.Parameters.AddWithValue("@Value" + i, StringProperty.Item2);
+                }
+            }
+            Connection.Open();
+            OleDbDataReader DataReader = SelectCommand.ExecuteReader();
+            List<Pupil> Pupils = new List<Pupil>();
+            while(DataReader.Read()){
+                //(string, object)[] PropertyAndValue = new (string, object)[DataReader.FieldCount];
+                Pupil CurrentPupil = new Pupil();
+                for(int i = 0; i < DataReader.FieldCount; i++){
+                    CurrentPupil.GetType().GetProperty(PUPIL_TABLE_COLUMN_NAMES[i]).SetValue(CurrentPupil, DataReader[i], null);
+                }
+                Pupils.Add(CurrentPupil);
+            }
+            Connection.Close();
+            return Pupils;
         }
         
 
         public override void WritePupilData(Pupil p_Pupil) {
-            throw new NotImplementedException();
+            OleDbConnection Connection = new OleDbConnection(CONNECTION_STRING_TEMPLATE + this.DatabasePath + "\\" + DATABASE_NAME);
+
+            OleDbCommand InsertCommand = new OleDbCommand();
+            InsertCommand.CommandType = CommandType.Text;
+            InsertCommand.CommandText = "INSERT INTO [Pupil] ([PupilUUID], [PupilID], [Name], [Company], [A2E], [ImgRef]) VALUES (@PupilUUID, @PupilID, @Name, @Company, @A2E, @ImgRef)";
+            InsertCommand.Parameters.AddWithValue("@PupilUUID", p_Pupil.PupilUUID);
+            InsertCommand.Parameters.AddWithValue("@PupilID", p_Pupil.PupilID);
+            InsertCommand.Parameters.AddWithValue("@Name", p_Pupil.Name);
+            InsertCommand.Parameters.AddWithValue("@Company", p_Pupil.Company);
+            InsertCommand.Parameters.AddWithValue("@A2E", p_Pupil.A2E);
+            InsertCommand.Parameters.AddWithValue("@ImgRef", p_Pupil.ImgRef);
+            InsertCommand.Connection = Connection;
+            //Also do Notes!!
+            Connection.Open();
+            InsertCommand.ExecuteNonQuery();
+            Connection.Close();
         }
     }
 }
