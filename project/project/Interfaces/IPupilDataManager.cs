d67dd951a6b33256e0a1b3dd8668a5cb997b828e
Permille <67966018+Permille@users.noreply.github.com>
2021-02-11 13:50:52 +0000

    Added new convenience method for making test cases

--- project/project/Interfaces/IPupilDataManager.cs
+++ project/project/Interfaces/IPupilDataManager.cs
@@ -22,5 +22,12 @@ namespace project.Interfaces {
         public abstract bool ConfirmPupilIDIntegrity();
         public abstract List<Pupil> GetPupilsByProperties(object Properties);
         public abstract void WritePupilData(Pupil p_Pupil);
+        public Pupil[] GetTestCases(int Count) {
+            if ((Count >> 8) > 0) throw new Exception("Really?");
+            Pupil[] PupilArray = new Pupil[Count];
+            Random v_Random = new Random();
+            for (int i = 0; i < Count; i++) PupilArray[i] = new Pupil(System.IO.Path.GetRandomFileName(), (v_Random.Next(0x00989680, 0x05f5e0ff) >> 0).ToString(), (v_Random.Next(0, 4) >> 1) > 1 ? "Tesco" : "Sainsbury's", v_Random.Next(0, 2) < 1, new List<Note>(){}, System.IO.Path.GetRandomFileName());
+            return PupilArray;
+        }
     }
 }
