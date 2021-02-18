2bf8e7add48a489b7b81c49ff79818ac33680fc1
Permille <67966018+Permille@users.noreply.github.com>
2021-02-18 12:13:05 +0000

    Support for getting and saving pupil images.

--- project/project/Interfaces/IPupilDataManager.cs
+++ project/project/Interfaces/IPupilDataManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static project.PupilDataManager.SharedResources.Types;
+using System.Drawing;
+using System.IO;
 
 namespace project.Interfaces {
     interface IPupilDataManager {
@@ -11,8 +13,13 @@ namespace project.Interfaces {
         bool ConfirmPupilIDIntegrity();
         List<Pupil> GetPupilsByProperties(object Properties);
         List<T> CollatePropertyValuesFromPupils<T>(string Property);
+        void SavePupilImage(Pupil p_Pupil, Image PupilImage);
+        Image GetPupilImage(Pupil v_Pupil);
     }
     public abstract class BasePupilDataManager : IPupilDataManager, IInstallable{
+        public static readonly string PUPIL_IMAGE_LOCATION = Environment.GetEnvironmentVariable("LocalAppData") + "\\PupilRecordsProgram\\PupilImages";
+        public static readonly string DEFAULT_PUPIL_IMAGE = "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAMCAIAAADQ/GvKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA9SURBVChTjYxBDgAgCMP4/6dxZAsCkcRenB1gvqDCCjLDklbEWMn6J++CFkSmAnJ5kw+oFtw0+NhYi4b7AdBYyzVvFnVtAAAAAElFTkSuQmCC";
+        public static readonly string IMAGE_FORMAT = "png";
         public BasePupilDataManager(string Location) {
             if (!this.CheckIfInstalled(Location)) this.Install(Location);
         }
@@ -28,6 +35,20 @@ namespace project.Interfaces {
             Random v_Random = new Random();
             for (int i = 0; i < Count; i++) PupilArray[i] = new Pupil(System.IO.Path.GetRandomFileName(), (v_Random.Next(0x00989680, 0x05f5e0ff) >> 0).ToString(), (v_Random.Next(0, 4) >> 1) > 1 ? "Tesco" : "Sainsbury's", v_Random.Next(0, 2) < 1, new List<Note>(){}, System.IO.Path.GetRandomFileName());
             return PupilArray;
+        }
+        public void SavePupilImage(Pupil p_Pupil, Image PupilImage){
+            PupilImage.Save(PUPIL_IMAGE_LOCATION + "\\" + p_Pupil.PupilUUID + "." + IMAGE_FORMAT);
+        }
+        public Image GetPupilImage(Pupil p_Pupil){
+            if(File.Exists(PUPIL_IMAGE_LOCATION + "\\" + p_Pupil.PupilUUID + "." + IMAGE_FORMAT)){
+                return Image.FromFile(PUPIL_IMAGE_LOCATION + "\\" + p_Pupil.PupilUUID + "." + IMAGE_FORMAT);
+            } else{
+                byte[] ImageBytes = Convert.FromBase64String(DEFAULT_PUPIL_IMAGE);
+                Image DefaultImage;
+                MemoryStream v_MemoryStream = new MemoryStream(ImageBytes);
+                DefaultImage = Image.FromStream(v_MemoryStream);
+                return DefaultImage;
+            }
         }
     }
 }
