7b12adafb0e7a9ddb9e4bbcac4761928ce572d78
Permille <67966018+Permille@users.noreply.github.com>
2021-02-25 12:09:07 +0000

    Utilities class

---
+++ project/project/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project {
+    public static class Utilities {
+        public static T GetReflect<T>(object Base, string Property) {
+            return (T)Base.GetType().GetProperty(Property).GetValue(Base, null);
+        }
+        public static void SetReflect<T>(object Base, string Property, T Value) {
+            Base.GetType().GetProperty(Property).SetValue(Base, Value, null);
+        }
+    }
+}
