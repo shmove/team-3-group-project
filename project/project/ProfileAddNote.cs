f6ee10dd47dbe08a7a66ac82c6da63ddc009f635
misfitbrit <matthewliddle03@gmail.com>
2021-03-19 20:06:50 +0000

    Fixed saving of invalid notes

--- project/project/ProfileAddNote.cs
+++ project/project/ProfileAddNote.cs
@@ -53,35 +53,45 @@ namespace project
 
         private void ButtonAddNote_Click(object sender, EventArgs e)
         {
-            // I'd really like to make newlines possible here, but it doesn't seem like that's gonna happen :(
-            string newNote = TextBoxNote.Text.Replace("\r", " ").Replace("\n", "");
 
-            switch (profileForm.noteContext)
+            if (TextBoxNote.Text.Trim() != "")
             {
-                case "add":
-                    string currentDate = DateTime.Now.ToString("d-M-yyyy");
-                    profileForm.activeStudent.Notes.Add(new Note(currentDate, newNote));
+                // I'd really like to make newlines possible here, but it doesn't seem like that's gonna happen :(
+                string newNote = TextBoxNote.Text.Replace("\r", " ").Replace("\n", "");
 
-                    break;
-                case "edit":
-                    // iterates through notes and overwrites initial note (probably could be done better)
-                    bool Found = false;
-                    foreach(Note i_Note in profileForm.activeStudent.Notes) if(i_Note.Text == initialNote){
-                        i_Note.Text = newNote;
-                        Found = true;
+                switch (profileForm.noteContext)
+                {
+                    case "add":
+                        string currentDate = DateTime.Now.ToString("d-M-yyyy");
+                        profileForm.activeStudent.Notes.Add(new Note(currentDate, newNote));
+
                         break;
-                    }
-                    if(!Found) throw new Exception("The specified note wasn't found.");
-                    break;
+                    case "edit":
+                        // iterates through notes and overwrites initial note (probably could be done better)
+                        bool Found = false;
+                        foreach (Note i_Note in profileForm.activeStudent.Notes) if (i_Note.Text == initialNote)
+                            {
+                                i_Note.Text = newNote;
+                                Found = true;
+                                break;
+                            }
+                        if (!Found) throw new Exception("The specified note wasn't found.");
+                        break;
+                }
+
+                profileForm.activeStudent.LastAccess = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.s"); // updates last accessed time
+
+                // Write new info to db
+                Mgr.WritePupilData(profileForm.activeStudent);
+                SystemSounds.Beep.Play();
+
+                this.Close();
             }
-
-            profileForm.activeStudent.LastAccess = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.s"); // updates last accessed time
-
-            // Write new info to db
-            Mgr.WritePupilData(profileForm.activeStudent);
-            SystemSounds.Beep.Play();
-
-            this.Close();
+            else
+            {
+                SystemSounds.Hand.Play();
+                MessageBox.Show("Please ensure note text is not empty.", "Invalid Note", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
         }
 
